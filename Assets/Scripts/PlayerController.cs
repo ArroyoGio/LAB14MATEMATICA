df99c178ad835508df38c11d3e495f375319076f
giovanna <giovanna.arroyo@tecsup.edu.pe>
2025-11-20 14:59:05 -0500

    move Player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movimiento")]
+    [SerializeField] private float speed;
+
+    [Header("Salto")]
+    [SerializeField] private float jumpForce;
+
+    private Vector2 input;
+    private bool isGrounded;
+    private Rigidbody rb;
+
+    private void Update()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        input = context.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(input.x, 0, input.y);
+        rb.MovePosition(rb.position + speed * direction * Time.deltaTime);
+    }
+
+
+}
